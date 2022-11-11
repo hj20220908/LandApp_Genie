2ac484941a88456057fc76b1339682c8e5eddae5
hj20220908 <hj20220908@gmail.com>
2022-11-11 11:15:24 +0900

    방목록 보여주기 (2) : datas 함수 활용

---
+++ app/src/main/java/com/example/landapp_genie/adapters/RoomAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.landapp_genie.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.landapp_genie.R
+import com.example.landapp_genie.datas.Room
+
+class RoomAdapter(val mContext : Context,
+                  val resId : Int,
+                  val mList : ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList){
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val roomData = mList[position]
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+        descriptionTxt.text = roomData.description
+        addressAndFloorTxt.text = "${roomData.address}, ${roomData.getFormattedFloor()}"
+
+        return row
+    }
+}
